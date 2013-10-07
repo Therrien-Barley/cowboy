2cd671f0b68e434092f7cf4849e09c674fea0ac5
provolot <provolot@gmail.com>
2013-10-06 20:12:13 -0400

    fixed git merge errors

--- eg/app.js
+++ eg/app.js
@@ -13,15 +13,11 @@ server.listen(3000);
 
 
 
-<<<<<<< HEAD
-var lou = new louis();
-=======
 var lou = new louis({
 	freq: 1000,
 	total: 10
 });
 
->>>>>>> 33ba859d5bb6180a9789f1bf62d7850ec62ffcda
 var boy = new cowboy();
 
 //start the test setInterval
